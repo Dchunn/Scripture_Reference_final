4f103a9b92f475e8a9dc6fae6bee2a7c8ea81119
Dchunn <chunn289@gmail.com>
2016-10-14 20:48:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/daniel/scripture_reference/Scripture.java
@@ -0,0 +1,13 @@
+package com.example.daniel.scripture_reference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scripture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scripture);
+    }
+}
